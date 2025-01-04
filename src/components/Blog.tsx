1a8541c073f3acb8a649815f9e0f9266d1bbe904
kristinasamuel <166210993+kristinasamuel@users.noreply.github.com>
2025-01-04 15:19:33 +0500

    Class assignment completed

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -18,14 +18,14 @@ export default async function BlogPage() {
       <h1 className="font-bold text-[38px] lg:text-[48px] text-yellow-400 mt-10 font-serif">
         Festivals of Pakistan
       </h1>
-      <p className="text-center font-medium text-[18px] sm:text-[18px] text-[white] lg:text-[22px] hover:underline underline-offset-8">
+      <p className="text-center font-medium text-[18px] sm:text-[18px] text-[#ffffff] lg:text-[22px] hover:underline underline-offset-8">
         Each festival in Pakistan tells a unique story.
       </p>
 
       {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-10 items-center justify-center p-4 h-auto"> */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 items-center justify-items-center p-4 h-auto max-w-6xl">
         {responce.map((data) => (
-          <Card key={data.title} className="shadow-md bg-gray-400 flex flex-col">
+          <Card key={data.title} className="shadow-md  flex flex-col">
             {data.poster && (
               <Image
                 src={urlFor(data.poster).url()}
@@ -36,7 +36,7 @@ export default async function BlogPage() {
               />
             )}
             <div className="flex-1 px-3 mt-2 space-y-3">
-              <h2 className="font-semibold text-[22px] text-sky-700">
+              <h2 className="font-semibold text-[22px] text-sky-500">
                 {data.title}
               </h2>
               <p className="text-[16px]">{data.description}</p>
@@ -47,8 +47,8 @@ export default async function BlogPage() {
           </Card>
         ))}
       </div>
-      <div className="mt-4 mb-5">
-        <p className="mt-20 text-yellow-400 text-[24px] text-center w-full lg:max-w-3xl">
+      <div className="mt-2 mb-5">
+        <p className="mt-20 text-yellow-300 text-[24px] text-center w-full lg:max-w-3xl">
           In Pakistan, festivals like melas and urs bring people together to
           celebrate our traditions. They show our country's religious and
           cultural unity.
